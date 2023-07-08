56fcbfdf6c5bf9dccbab59408d5d3daffe0fb7ac
arsaizdihar <arsadihar@gmail.com>
2023-07-08 15:06:14 +0700

    Add OpenAPI docs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestJsSwagger } from 'nestjs-zod';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http/http-exception.filter';
 import { ResponseInterceptor } from './http/response-interceptor';
@@ -10,8 +12,16 @@ async function bootstrap() {
     new HttpExceptionFilter(),
     new ZodValidationExceptionFilter(),
   );
+  const config = new DocumentBuilder()
+    .setTitle('OHL Single')
+    .setDescription('Untuk keperluan seleksi asisten labpro')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.enableCors({ origin: '*' });
   await app.listen(process.env.PORT ?? 3000);
 }
+patchNestJsSwagger();
 bootstrap();
